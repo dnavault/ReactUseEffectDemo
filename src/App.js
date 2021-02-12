799f43d4d13fcf9511c74b3c34730451bb215f50
dnavault <47720676+dnavault@users.noreply.github.com>
2021-02-12 15:50:56 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+ 
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+ 
+      setIsLoading(false);
+    };
+ 
+    fetchData();
+  }, [url]);
+ 
+  return [{ data, isLoading, isError }, setUrl];
+};
+ 
+function App() {
+  const [query, setQuery] = useState('');
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    'https://api.hatchways.io/assessment/work_orders',
+    { orders: []},
+  );
+  
+  return (
+    
+    <Fragment>
+      <body>
+      <form
+        onSubmit={event => {
+          doFetch(
+            `https://api.hatchways.io/assessment/workers/1`,
+        {orders: [data.worker]}
+          );
+           
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+ 
+      {isError && <div>Something went wrongg ...</div>}
+ 
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+       <>
+{console.log(data.orders)}
+        <ul>
+          
+        
+          {data.orders.map(item => (
+            <li key={item.objectID}>
+              <a href={item.name}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+       
+      </>
+      )}
+      </body>
+    </Fragment>
+  );
+}
+ 
+export default App
